23ce59b71aec8afb833b63ce435e7cc07d830f3a
Mevine  Akinyi <72660182+mevinedev@users.noreply.github.com>
2023-05-10 17:00:11 +0300

    loops using javascript

---
+++ loops.js
@@ -0,0 +1,25 @@
+// for loops.
+// Three statements on one line.
+for (var i = 0; i < 10; i++) {
+    console.log('jambo', i);
+  }
+  
+  // while loops.
+  // Execute while a condition is true.
+  // Like a for loop, except the statements are on individual liens
+  var j = 0;
+  
+  while (j < 10) {
+    console.log('hakuna matata', j);
+  
+    j++;
+  }
+  
+  var a = 15;
+  
+  // do while loops
+  // Like a while loop, except the body executes at least once
+  do {
+    console.log('a is', a);
+    a++;
+  } while (a < 10);
